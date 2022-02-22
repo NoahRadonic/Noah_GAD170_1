9651f4389fd19563037f55ce033db53ddd3f00ce
NoahRadonic <1027512@student.sae.edu.au>
2022-02-22 23:10:33 +1100

    XP Work

--- Assets/XPsystem.cs
+++ Assets/XPsystem.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class XPsystem : MonoBehaviour
 {
-    public int level; //laps
-    public float curEXP; //curLapProgress
-    public float reqEXP; //reqLapProgress
+    public int level = 0; 
+    //laps
+    public float curEXP = 0f; 
+    //curLapProgress
+    public float reqEXP = 100f; 
+    //reqLapProgress
+
+
     //Stats
-    public float health;
-    public float defense;
-    public float speed;
-    public float attack;
+    public float health = 100;
+    public float defense = 10;
+    public float speed = 5;
+    public float attack = 15;
 
     // Start is called before the first frame update
     void Start()
@@ -28,11 +33,18 @@ public class XPsystem : MonoBehaviour
     public void InitialStats(float earnedEXP)
     {
         //This is called by button presses, should grant diff amount of exp
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GainXP (speed)
+        }
     }
 
     public void GainXP(float gain)
     {
         //gain exp, probably something to do with our curEXP
+        curEXP += gain
+        Debug.Log("EXP Gained : " + gain)
+        Debug.Log("Current EXP Gained : " + curEXP)
     }
 
     public void Levelup()
