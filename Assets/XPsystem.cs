3e75222038fefecec2ac1c4aadaf078de2804d45
NoahRadonic <1027512@student.sae.edu.au>
2022-03-02 09:41:16 +1100

    YEEEE

--- Assets/XPsystem.cs
+++ Assets/XPsystem.cs
@@ -8,35 +8,46 @@ public class XPsystem : MonoBehaviour
     //laps
     public float curEXP = 0f; 
     //curLapProgress
-    public float reqEXP = 100f; 
+    public float reqEXP = 1000f; 
     //reqLapProgress
 
 
     //Stats
     public float health = 100;
-    public float defense = 10;
+    public float defense = 1;
     public float speed = 5;
-    public float attack = 15;
+    public float attack = 5;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
-    {
-        
-    }
-
-    public void InitialStats(float earnedEXP)
     {
         //This is called by button presses, should grant diff amount of exp
         if (Input.GetKeyDown(KeyCode.Space))
         {
             GainXP(speed);
         }
+
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            GainXP(attack);
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            GainXP(defense);
+        }
+
+    }
+
+    public void InitialStats(float earnedEXP)
+    {
+       //stats, levels and xp !
     }
 
     public void GainXP(float gain)
@@ -45,17 +56,32 @@ public class XPsystem : MonoBehaviour
         curEXP += gain;
         Debug.Log("EXP Gained : " + gain);
         Debug.Log("Current EXP Gained : " + curEXP);
+
+        Levelup();
     }
 
     public void Levelup()
     {
         //increase our level
-        
-        
+        if (curEXP >= reqEXP)
+        {
+            IncreaseStats();
+            Debug.Log("U FOOKIN LEVELED THE UP"); 
+        }
     }
 
     public void IncreaseStats()
     {
         //increase our various stats
+        //Need increase level, reset xp, and increase reqxp peepee
+        level += 1;
+        curEXP = 0;
+        reqEXP *= 10;
+
+        health += 5f * (0.5f * (level ^ 2));
+        defense += 1f * (0.5f * (level ^ 2));
+        speed += 5f * (0.5f * (level ^ 2));
+        attack += 5f * (0.5f * (level ^ 2));
+        Debug.Log("You are now Level :" + level + "!!!");
     }
 }
